35e84173d51da053a94e72ce3e50048186722312
quentin.rma <quentin.romera@outlook.fr>
2025-02-08 17:09:30 +0100

    Add favicon and update header with Prototypes link; create Prototypes page

--- components/Header.js
+++ components/Header.js
@@ -10,6 +10,7 @@ document.write(`
         <ul>
           <li><a href="../index.html">Accueil</a></li> 
                   <li><a href="../pages/CV.html">CV</a></li>
+          <li><a href="../pages/Prototypes.html">Prototypes</a></li>
 
 
         </ul>
